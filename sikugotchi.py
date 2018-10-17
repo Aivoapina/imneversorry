8c9dcf740637846b3f07f15b6d7d3bf363bb2be5
Jaakko Husso <jaakko.husso@futurice.com>
2018-10-17 23:17:38 +0300

    Make siku names kurkkumopo names

--- sikugotchi.py
+++ sikugotchi.py
@@ -1,4 +1,7 @@
 import requests
+import db
+import random
+
 from configparser import ConfigParser
 
 cfg = ConfigParser()
@@ -12,14 +15,22 @@ class Sikugotchi:
                           'delsiku': self.delSikuHandler,
                           'feedsiku': self.feedSikuHandler,
                           'sikumap': self.sikuMapHandler }
+        self.vihanneet = db.readVihanneet()
+        self.planetoidit = db.readPlanetoidit()
+        self.kulkuneuvot = db.readKulkuneuvot()
 
     def getCommands(self):
         return self.commands
 
     def addSikuHandler(self, bot, update, args):
+        kurkku = random.sample(self.vihanneet, 1)[0][0]
+        mopo = random.sample(self.kulkuneuvot, 1)[0][0]
+        kuu = random.sample(self.planetoidit, 1)[0][0]
+        sikuNimi = kurkku + ("", "-")[kurkku[-1:] == mopo[0] and mopo[0] in ('a', 'e', 'i', 'o', 'u', 'y', 'ä', 'ö')] + mopo + " eli " + kuu + ("", "-")[kuu[-1:] == 'e'] + 'eläin ' + kurkku + 'maasta'
+
         r = requests.post(
             'https://sikus.sivu.website/api/v1/siku/add',
-            json={'siku': {'name': 'Siku', 'creator': update.message.from_user.username}},
+            json={'siku': {'name': sikuNimi, 'creator': update.message.from_user.username}},
             headers=headers
         )
         if r.status_code == 201:
