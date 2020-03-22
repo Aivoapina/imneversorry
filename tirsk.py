915f0986c44d6e14683a7bd5662e491953e44b66
Pauli Oikkonen <pauli.oikkonen@tuni.fi>
2020-03-22 15:59:59 +0200

    (tirsk)

---
+++ tirsk.py
@@ -0,0 +1,22 @@
+import random
+
+class Tirsk:
+    isTirsk  = lambda self: random.random() < self.tirsk_prob
+    rndTirsk = lambda self: random.choice(("tirsk", "Tirsk", "tirsk :D", "(tirsk)", "[tirsk]"))
+
+    def __init__(self, tirsk_prob = 0.0001):
+        self.tirsk_prob = tirsk_prob
+
+    def getCommands(self):
+        return dict()
+    def tirskTime(self):
+        return random.randint(0, self.tirsk_prob) == 0
+
+    def sendTirsk(self, bot, update):
+        chat_id = update.message.chat.id
+        bot.sendMessage(chat_id=chat_id, text=self.rndTirsk())
+
+    def messageHandler(self, bot, update):
+        msg = update.message
+        if self.isTirsk():
+            self.sendTirsk(bot, update)
