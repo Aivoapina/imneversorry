295207cb62bebd3672825a2f71be958307f82815
Lassi Väisänen <kLassikko@users.noreply.github.com>
2020-08-08 12:40:00 +0300

    multi-vai implementation

--- valitsin.py
+++ valitsin.py
@@ -11,15 +11,14 @@ class Valitsin:
     def getCommands(self):
         return self.commands
 
-    def makeDecision(self, bot, update, groups):
+    def makeDecision(self, bot, update, alternatives):
         now = datetime.datetime.now()
         data = [
             update.message.from_user.id,
             now.day,
             now.month,
             now.year,
-            groups.group(1),
-            groups.group(2)
+            alternatives
         ]
         seed = hashlib.md5(json.dumps(data, sort_keys=True).encode('utf-8')).hexdigest()
         rigged = random.Random(seed)
@@ -27,7 +26,7 @@ class Valitsin:
             answers = ['Molemmat :D', 'Ei kumpaakaan >:(']
             bot.sendMessage(chat_id=update.message.chat_id, text=rigged.sample(answers, 1)[0])
         else:
-            bot.sendMessage(chat_id=update.message.chat_id, text=rigged.sample([groups.group(1), groups.group(2)], 1)[0])
+            bot.sendMessage(chat_id=update.message.chat_id, text=rigged.sample(alternatives, 1)[0])
 
     def onkoPakko(self, bot, update, groups):
         now = datetime.datetime.now()
@@ -48,9 +47,9 @@ class Valitsin:
     def messageHandler(self, bot, update):
         msg = update.message
         if msg.text is not None:
-            vai = re.match(r"(^\S+) vai (\S+)$", msg.text.lower())
+            vai = set(msg.text.lower().split()[1::2]) == {'vai'}
             pakko = re.match(r"^onko pakko ([^?]+)(\??)$", msg.text.lower(), re.IGNORECASE)
             if vai:
-                self.makeDecision(bot, update, vai)
+                self.makeDecision(bot, update, msg.text.lower().split()[::2])
             elif pakko:
                 self.onkoPakko(bot, update, pakko)
